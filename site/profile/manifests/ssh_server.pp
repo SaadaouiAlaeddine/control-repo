32fe08bc4afe1ed5a99ed14b3c9886572ab369b6
SaadaouiAlaeddine <3alaeddine@gmail.com>
2018-03-05 13:05:22 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,4 +12,5 @@ class profile::ssh_server {
   type => 'ssh-rsa',
   key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDLZbiYNWYlJXdVCDWyE9iw+HVPIQSw9cbTuWCfszKv8kjjLP1ciwUbcopq381/tTMIgLSCdZ+NMofwUPVhF/jieegSZIOhHM2R+8sVOIN+FT2jeH3UgjD1yNqigIdAST3nMWndFsTVEzQyYNEKLS9i1y3fD1PcpnhsnkYosHwfCCj9LZnJoMj/7bCv0nxI+9GSKo3WN+bLU6S6ypRRJu0ingyvkOGvb8OzIch5s2MbkiwK/mEs9MZndB/lr8ErBUwlSs1PeDG480YjEAcfeoSs/IURS0fQ8XU7m3vrNPwfu+e+3TnUauWg4tOlIrQLOsOFlBrj8IWU5wkXVQkZzoU3',
  }
+}
   
